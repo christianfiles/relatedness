be1cf16f443e9cc5ddda1886b39386906b0a3e6f
josephhalstead <josephhalstead89@gmail.com>
2021-09-09 17:09:07 +0100

    changes to allow siblings

--- relatedness2.py
+++ relatedness2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import pandas as pd
+import csv
+
 """
 relatedness.py
 
@@ -7,93 +10,219 @@ Calculates whether the relationships between proband and parents are what is exp
 
 """
 
+def get_relatedness(df, sample1, sample2):
+	"""
+	Given a df on the relatedness2 file return the relatedness value
+	
+	df (pd.DataFrame): Pandas dataframe of relatedness2 file
+	sample1 (str): A sample id
+	sample2 (str): A sample id
+	
+	Returns float or None
+	"""
+	rows = df[(df['INDV1'] == sample1) & (df['INDV2'] == sample2)]
+	
+	if rows.shape[0] == 0:
+		
+		return None
+	
+	else:
+		
+		return rows['RELATEDNESS_PHI'].iloc[0]
+	
+def check_relatedness_parent(df, sample_id, parent_id, min_relatedness_parents, max_relatedness_parents):
+	"""
+	Check the relatedness between a parents of a sample.
+	
+	df (pd.DataFrame): Pandas dataframe of relatedness2 file
+	sample_id (str): Proband id
+	parent_id (str): The parent id
+	min_relatedness_parents (float): See relatedness_test
+	max_relatedness_parents (float): See relatedness_test
+	
+	Return True if the relatedness is not between min_relatedness_parents and max_relatedness_parents
+	
+	"""
+	
+	# ignore if parent is undefined
+	if parent_id not in [0, '0']:
+		
+		relatedness_parent = get_relatedness(df, sample_id, parent_id)
 
-import pandas as pd
+		if relatedness_parent is None:
 
-def relatedness_test(ped, relatedness_file, min_relatedness, max_relatedness, max_parents_relatedness):
+			return False
 
-    """
-    Open the ped and relatedness files
-    Assigns appropriate variables to sampleID from ped file
-    Removes NTC from relatedness file
-    Compares the sampleID variables from ped against the values in the relatedness file
-    
-    """
-    
-    # Try opening the ped and relatedness files, otherwise output that the file could not be found
-    try:
-        ped_file = pd.read_csv(ped, delimiter = '\t', header = None)
-        relate_file = pd.read_csv(relatedness_file, delimiter = '\t')
-    except:
-        return False, 'Either the pedigree file or relatedness file could not be found'
+		else:
 
-    # Check to see if there are the correct amount of columns in each file and if yes then rename column names in ped file
-    if len(relate_file.columns) != 7:
-        return False, 'There are the incorrect amount of columns within the relatedness file'
+			# check between min and max relatedness
+			if not min_relatedness_parents <= relatedness_parent <= max_relatedness_parents:
 
-    # This section creates a unique list of family IDs from the ped file, therefore it works with multiple families
-    if len(ped_file.columns) != 6:
-        return False, 'There are the incorrect amount of columns within the pedigree file'
-    else:
-        ped_file.columns = ['family_id', 'individual_id', 'paternal_id', 'maternal_id', 'sex', 'phenotype']
-    
-    family_identifier = ped_file.family_id.unique().tolist()
+				return True
+			
+			
+	return False
 
-    if '0' in family_identifier:
+def get_family_and_sample_dicts(ped):
+	"""
+	Parse the ped file and create two diction
+	
+	ped (str): Path to PED file
+	
+	Returns two dictionaries
+	"""
 
-        family_identifier.remove('0')
+	sample_dict = {}
+	
+	family_dict = {'singletons': []}
+	
+	# open ped
+	with open(ped) as csvfile:
+		
+		spamreader = csv.reader(csvfile, delimiter='\t')
+		
+		# loop through each row
+		for row in spamreader:
+			
+			fam_id = row[0]
+			sample_id = row[1]
+			dad_id = row[2]
+			mum_id = row[3]
+			
+			# make family dict which links samples to a family
+			if fam_id == 0 or fam_id == '0':
+				
+				family_dict['singletons'].append(sample_id)
+			
+			else:
+				
+				if fam_id not in family_dict:
+					
+					family_dict[fam_id] = [sample_id]
+					
+				else:
+					
+					family_dict[fam_id].append(sample_id)
+					
+					
+			# create sample dict which links samples to relatives
+			if sample_id in sample_dict:
+				
+				raise Exception('Duplicate sample in PED file.')
+			
+			sample_dict[sample_id] = {'mum': mum_id, 'dad': dad_id, 'family_id': fam_id}
 
-    elif 0 in family_identifier:
+			
+	return sample_dict, family_dict
 
-        family_identifier.remove(0)
+def relatedness_test(ped, relatedness_file, min_relatedness_parents, max_relatedness_unrelated, max_relatedness_between_parents, max_child_parent_relatedness):
+	"""
+	Completes a relatedness test given a ped file and a relatedness2 file.
+	
+	Checks for:
+	
+	1) All samples with parents have a relatedness between min_relatedness_parents and max_child_parent_relatedness
+	2) No unrelated samples have a relatedness larger than max_relatedness_unrelated
+	3) No parents have a relatedness larger than max_relatedness_between_parents
+	
+	ped (str): Path to ped file
+	relatedness_file (str): Path to relatedness2 file
+	min_relatedness_parents (float): A child and their parent must have a higher relatedness than this value
+	max_relatedness_unrelated (float): Two unrelated samples cannot have a higher relatedness than this value
+	max_relatedness_between_parents (float): Parents of a sample should not have higher relatedness than this value
+	max_child_parent_relatedness (float): A sample should not be related to their parent by more than this value
+	
+	returns a tuple (Boolean, list)
+	
+	where the list contains strings describing what went wrong and the boolean = True for pass, False for fail
+	"""
+	
+	fails = []
+	
+	# Try opening the ped file
+	try:
+		
+		relatedness_df = pd.read_csv(relatedness_file, delimiter = '\t')
+		
+	except:
+		
+		fails.append('The relatedness file could not be found.')
+	
+	# now create a dictionary with each sample as a key
+	try:
+		
+		sample_dict, family_dict =  get_family_and_sample_dicts(ped)
+		
+	except:
+		
+		fails.append('The pedigree file could not be parsed.')
+		return False, fails
+		
+	for sample_id in sample_dict:
+		
+		# check sample is related to their parent (if they have one)
+		fam_id = sample_dict[sample_id]['family_id'] 
+		mum_id = sample_dict[sample_id]['mum'] 
+		dad_id = sample_dict[sample_id]['dad']
+		
+		same_family = family_dict.get(fam_id, [])
+		
+		# check sample is related to parents
+		for parent_id in [mum_id, dad_id]:
+			
+			relatedness_status = check_relatedness_parent(relatedness_df, sample_id, parent_id, min_relatedness_parents, max_child_parent_relatedness )
+			
+			if relatedness_status:
+				
+				fails.append(f'sample {sample_id} invalid relatedness to parent {parent_id}.')
+				
+		# check for duplicate samples i.e. samples not in same family which have > max_relatedness_unrelated
+		for other_sample in sample_dict:
 
-    # Checking that family_identifier is populated
-    if family_identifier == []:
-        return False, 'Looks like family ID in the pedigree file is incorrect'
+			if other_sample not in same_family and other_sample != sample_id:
+							
+				other_sample_relatedness = get_relatedness(relatedness_df, sample_id, other_sample)
 
-    # Creating an empty dictionary to store family identifiers and also their variables (key value pairs)
-    family_id = {}
+				if other_sample_relatedness is not None:
+				
+					if other_sample_relatedness > max_relatedness_unrelated:
+					
+						fails.append(f'{sample_id} is related to non family member {other_sample}')
 
-    for ID in family_identifier:
-        family_id[ID] = {}
+				else:
 
-    # This will assign the mum for each ID
-    for ID in family_identifier:                                                                      
-       for x in ped_file.index:                                                                       
-            if ped_file.paternal_id[x] == '0' and ped_file.sex[x] == 2 and ped_file.family_id[x] == ID:
-                family_id[ID]['mum'] = ped_file.individual_id[x]
-            elif ped_file.paternal_id[x] == '0' and ped_file.sex[x] == 1 and ped_file.family_id[x] == ID:
-                family_id[ID]['dad'] = ped_file.individual_id[x]
-            elif ped_file.paternal_id[x] != '0' and ped_file.sex[x] != 0 and ped_file.family_id[x] == ID:
-                family_id[ID]['proband'] = ped_file.individual_id[x]
-            if ped_file.sex[x] > 2 or ped_file.phenotype[x] > 2:
-                return False, 'Pedigree file does not look correct'
-
-
-    # Getting rid of the NTCs in INDV columns
-    no_ntc = relate_file[relate_file['INDV1'] != 'NTC']
-    relate_file = no_ntc[no_ntc['INDV2'] != 'NTC']
-
-
-    for ID in family_identifier:
-        for x in relate_file.index:
-            if relate_file['INDV1'][x] == family_id[ID]['proband'] or relate_file['INDV2'][x] == family_id[ID]['proband']:
-                if relate_file['INDV1'][x] == family_id[ID]['mum'] or relate_file['INDV1'][x] == family_id[ID]['dad'] or relate_file['INDV2'][x] == family_id[ID]['mum'] or relate_file['INDV2'][x] == family_id[ID]['dad']:
-                    if relate_file['RELATEDNESS_PHI'][x] >= min_relatedness and relate_file['RELATEDNESS_PHI'][x] <= max_relatedness:
-                        pass
-                    elif relate_file['RELATEDNESS_PHI'][x] < min_relatedness or relate_file['RELATEDNESS_PHI'][x] > max_relatedness:
-                        return False, 'Relatedness error'
-            if relate_file['INDV1'][x] == family_id[ID]['mum'] and relate_file['INDV2'][x] == family_id[ID]['dad'] and relate_file['RELATEDNESS_PHI'][x] >= max_parents_relatedness:
-                return False, 'Looks like a possible problem. Check pedigree and relateness file'
-        return True, 'Everything relatedness looks fine'
+					fails.append(f'Could not retrieve relatedness between {sample_id} and {other_sample}')
+		
+		# check parents are not related
+		relatedness_parents = get_relatedness(relatedness_df, mum_id, dad_id)
+		
+		if relatedness_parents is not None:
+			
+			if relatedness_parents > max_relatedness_between_parents:
+				
+				fails.append(f'{sample_id} parents are related to each other.')
+	
+	# if fails list in empty  then no issues	
+	if not fails:
+		
+		return True, ['No relatedness errors.']
+	
+	else:
+		
+		return False, fails
 
 if __name__ == '__main__':
 
-    #result = relatedness_test('testData/210622_A00748_0110_AH5T3CDRXY.ped', 'testData/210622_A00748_0110_AH5T3CDRXYFAIL.relatedness2')
-    #result = relatedness_test('testData/201215_A00748_0068_AHT3FCDMXX.ped', 'testData/201215_A00748_0068_AHT3FCDMXX.relatedness2', 0.2, 0.3, 0.04)
-    result = relatedness_test('testData/K00150_0149_AHG7YKBBXX.ped', 'testData/K00150_0149_AHG7YKBBXX.relatedness2', 0.2, 0.3, 0.04)
-    #result = relatedness_test('this_file_does_not_exist.ped', 'this_file_also_does_not_exist.relatedness2')
-    print(result)
+	ped = 'test_data/K00150_0149_AHG7YKBBXX.ped'
+	relatedness2 = 'test_data/K00150_0149_AHG7YKBBXX.relatedness2'
+
+	min_relatedness_parents = 0.2
+	max_relatedness_unrelated = 0.06
+	max_relatedness_between_parents = 0.06
+	max_child_parent_relatedness = 0.4
+
+	result = relatedness_test(ped, relatedness2, min_relatedness_parents, max_relatedness_unrelated, max_relatedness_between_parents, max_child_parent_relatedness  )
+	print(result)
 
 
 
